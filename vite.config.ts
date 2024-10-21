e9c55168867ff08efc4bfd20bf55db0a9086eb90
Avyukt27 <164896225+Avyukt27@users.noreply.github.com>
2024-10-21 22:18:02 +0530

    Prepare for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Rock-Paper-Scissors-Vue/',
+  base: "/Rock-Paper-Scissors-Vue/",
   plugins: [vue()],
 })
